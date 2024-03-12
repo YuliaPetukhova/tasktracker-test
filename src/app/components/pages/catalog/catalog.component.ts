2cc2bd498b0006f256069b1633cf76d18ea7424d
YuliaPetukhova <petukhova.yulia99@gmail.com>
2024-03-12 13:24:34 +0500

    таблица с фильтром и сортировкой

--- src/app/components/pages/catalog/catalog.component.ts
+++ src/app/components/pages/catalog/catalog.component.ts
@@ -1,17 +1,89 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild, AfterViewInit} from '@angular/core';
 import {RouterOutlet} from "@angular/router";
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSort, MatSortModule} from '@angular/material/sort';
+
+
+export interface UserData {
+  id: string;
+  name: string;
+  progress: string;
+  fruit: string;
+}
+
+const FRUITS: string[] = [
+  'blueberry',
+  'lychee',
+  'kiwi',
+  'mango',
+  'peach',
+  'lime',
+  'pomegranate',
+  'pineapple',
+];
+const NAMES: string[] = [
+  'Maia',
+  'Asher',
+  'Olivia',
+  'Atticus',
+  'Amelia',
+  'Jack',
+  'Charlotte',
+  'Theodore',
+  'Isla',
+
+];
 
 @Component({
   selector: 'app-catalog',
   standalone: true,
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatSortModule,
   ],
   templateUrl: './catalog.component.html',
-  styleUrl: './catalog.component.scss'
+  styleUrl: './catalog.component.css'
 })
-export class CatalogComponent {
+export class CatalogComponent implements AfterViewInit {
+  displayedColumns: string[] = ['id', 'name', 'progress', 'fruit'];
+  dataSource: MatTableDataSource<UserData>;
 
+  @ViewChild(MatSort, {static: true}) sort!: MatSort;
+
+  constructor() {
+    const users = Array.from({length: 10}, (_, k) => createNewUser(k + 1));
+
+    this.dataSource = new MatTableDataSource(users);
+  }
+
+  ngAfterViewInit() {
+    console.log(this.sort);
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
 
+function createNewUser(id: number): UserData {
+  const name =
+    NAMES[Math.round(Math.random() * (NAMES.length - 1))] +
+    ' ' +
+    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) +
+    '.';
+
+  return {
+    id: id.toString(),
+    name: name,
+    progress: Math.round(Math.random() * 100).toString(),
+    fruit: FRUITS[Math.round(Math.random() * (FRUITS.length - 1))],
+  };
+}
 
