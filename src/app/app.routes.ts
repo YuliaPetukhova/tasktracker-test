2d206b7ebeab0dee1d977f60a9d500c24156b205
YuliaPetukhova <petukhova.yulia99@gmail.com>
2024-04-12 17:02:28 +0500

    добавлены сообщения об ошибке при заполнении полей формы

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,11 @@ import {NgModule} from '@angular/core';
 export const routes: Routes = [
   {
     path: 'task/:id',
-    loadComponent: () => import('./components/pages/task-page/task-page.component').then(m => m.TaskPageComponent)
+    loadComponent: () => import("./components/pages/task-page/task-page.component").then(m => m.TaskPageComponent),
   },
   {
     path: 'catalog',
-    loadComponent: () => import('./components/pages/catalog/catalog.component').then(m => m.CatalogComponent)
+    loadComponent: () => import("./components/pages/catalog/catalog.component").then(m => m.CatalogComponent),
   },
 ];
 
@@ -16,4 +16,5 @@ export const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutes {}
+export class AppRoutes {
+}
